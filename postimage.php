db331c9ff2af80756e5028f36cab4631fb1cff2c
Sium <Sium@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-23 19:26:44 +0000

    new files
    git-svn-id: https://plugins.svn.wordpress.org/simple-image-upload/trunk@643770 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ postimage.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: Simple Image Upload
+Description: This mod integrates image hosting with WordPress
+Version: 1.4.0
+Requires at least: WP 3.0.0
+Tested up to: WP 3.5
+License: GNU General Public License 2.0 (GPL) http://www.gnu.org/licenses/gpl.html
+Author: Sium
+Author URI: http://postimage.org/
+Plugin URI: http://postimage.org/mod.php
+Last Modified: 18 December, 2012
+*/
+
+class postimage_header
+{
+	public static function postimage_get()
+	{
+		$postimage_js=plugins_url('wordpress-english.js');
+		
+		echo "<script type='text/javascript' src='{$postimage_js}' charset='utf-8'></script>\n";
+	}
+}
+
+add_action('admin_head',array('postimage_header','postimage_get'));
+
+add_action('wp_head',array('postimage_header','postimage_get'));
+
+?>
